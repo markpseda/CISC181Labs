d61659a52e38140acb38f5248f81433904304e63
Mark Seda <markpseda@gmail.com>
2016-02-17 09:56:15 -0500

    added thing

--- Lab1/src/MainPackage/Main.java
+++ Lab1/src/MainPackage/Main.java
@@ -3,8 +3,11 @@ package MainPackage;
 public class Main {
 
 	public static void main(String[] args) {
-			
+
 		SedaM.HelloWorld();
 	}
 
+	private static void calculateInterest() {
+		// do stuff
+	}
 }
