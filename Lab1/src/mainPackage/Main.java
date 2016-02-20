0971c8a31c85051c51182e2c6ee0a5e074304202
Mark Seda <markpseda@gmail.com>
2016-02-20 17:57:16 -0500

    Just a Main class now. We can add more if that makes it easier

--- Lab1/src/mainPackage/Main.java
+++ Lab1/src/mainPackage/Main.java
@@ -1,14 +1,26 @@
 package mainPackage;
-
+import java.util.Scanner;
+/* This program is designed to determine how much one should
+ * save to retire for a given amount of time given....
+ */
 public class Main {
 
 	public static void main(String[] args) {
 
-		SedaM.HelloWorld();
 		calculateInterest();
+		determineValues();
 	}
 
 	private static void calculateInterest() {
 		// do stuffs
+	}
+	
+	private static void determineValues(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = input.nextInt();
+		System.out.println(a);
+		input.close();
+		
 	}
 }
--- Lab1/src/mainPackage/SedaM.java
+++
@@ -1,9 +0,0 @@
-package mainPackage;
-
-public class SedaM {
-	public static void HelloWorld(){
-		
-		System.out.println("Hello World");
-	}
-
-}
