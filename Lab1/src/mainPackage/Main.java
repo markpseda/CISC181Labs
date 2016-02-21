4de8b777d74a92859890ad1bbe3618e1bb57bc8b
Mark Seda <markpseda@gmail.com>
2016-02-21 16:31:55 -0500

    Basically done. Just need better comments.

--- Lab1/src/mainPackage/Main.java
+++ Lab1/src/mainPackage/Main.java
@@ -2,20 +2,21 @@ package mainPackage;
 import java.util.Scanner;
 /* This program is designed to determine how much one should
  * save to retire for a given amount of time given....
+ * 
+ * 
+ *  TotalSaving = (RequiredIncome-MonthlySSIncome)*((1-(1/(Math.pow(1+(AnnualRetirment/100)/12,RetiredYears*12 )))))/((AnnualRetirment/100)/12);
+        SaveMonthly = TotalSaving*(((AnnualReturn/100)/12)/((Math.pow(1+(AnnualReturn/100)/12, numberOfYears*12))-1));
+ * 
+ * 
  */
 public class Main {
 
 	public static void main(String[] args) { //Main process
 		
 		determineValues();
-		calculateInterest();
-		determineValues();
+
 	}
 
-	private static void calculateInterest() {
-		// do stuffs
-	}
-	
 	private static void determineValues(){ //Used to get all needed input values
 		Scanner input = new Scanner(System.in);
 		
@@ -23,20 +24,35 @@ public class Main {
 		int numYearsWork = input.nextInt();
 		
 		System.out.println("Enter annual return for your working years.");
-		int workingReturn = input.nextInt();
+		double workingReturn = input.nextDouble();
 		
 		System.out.println("Enter the number of years you plan to retire.");
 		int numYearsRetire = input.nextInt();
 		
 		System.out.println("Enter annual return for your retired years.");
-		int retiredReturn = input.nextInt();
+		double retiredReturn = input.nextDouble();
 		
 		System.out.println("Enter amount needed per year during retirement.");
-		int amountNeeded = input.nextInt();
+		double amountNeeded = input.nextDouble();
 		
 		System.out.println("Enter expected Social Security income.");
-		int SSI = input.nextInt();
+		double SSI = input.nextDouble();
 		
 		input.close();	
+		
+		calculateOutput(numYearsWork, workingReturn, numYearsRetire, retiredReturn, amountNeeded, SSI);
+	}
+	
+	private static void calculateOutput(int numYearsWork, double workingReturn, int numYearsRetire, double retiredReturn, double amountNeeded, double SSI) {
+		
+		double totalSave = (amountNeeded-SSI)*((1-(1/(Math.pow(1+(retiredReturn/100)/12,numYearsRetire*12 )))))/((retiredReturn/100)/12);
+		
+		double monthlySave = totalSave*(((workingReturn/100)/12)/((Math.pow(1+(workingReturn/100)/12, numYearsWork*12))-1));
+		System.out.print("You will need to save $");
+		System.out.printf("%.2f",totalSave);
+		System.out.println(" in all.");
+		System.out.print("This means you must save $");
+		System.out.printf("%.2f",monthlySave);
+		System.out.println(" per month.");
 	}
 }
